6fd4992cb6ec965ecf648cbf83d51e96adac5030
rohail jan <rohailjan726@gmail.com>
2025-01-29 15:30:23 +0500

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Banner from '../components/Banner'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import Work from '../components/Work'
+import Education from '../components/Education'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
        <Skills/>
        <Projects/>
        <Work/>
+       <Education/>
 
     </div>
   )
